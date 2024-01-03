0ce37316d3c77e355a1a3432d9f13166f4d0ce24
langpineiro <alangp@miumg.edu.gt>
2024-01-03 11:37:01 -0600

    add gitignore

--- src/main.py
+++ src/main.py
@@ -4,12 +4,12 @@ import poblacion_por_pais
 
 def run():
    res =  leer_csv.read_csv('./src/data.csv')
-#    opc =  input("Digita un pais:")
-#    pais = poblacion_por_pais.search_country(opc, res)
-#    if len(pais) > 0:
-#     graficar_info.generate_chart_bars(pais.keys(), pais.values())
-# print("No se encontro el pais")
-   country, per = poblacion_por_pais.generate_world_population(res)
-   graficar_info.generate_chart_pie(country, per)
+   opc =  input("Digita un pais:")
+   pais = poblacion_por_pais.search_country(opc, res)
+   if len(pais) > 0:
+    graficar_info.generate_chart_bars(pais.keys(), pais.values())
+print("No se encontro el pais")
+   # country, per = poblacion_por_pais.generate_world_population(res)
+   # graficar_info.generate_chart_pie(country, per)
 if __name__ == '__main__':
    run()
